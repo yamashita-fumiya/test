96442e6775dac6d85e6bd0e799db425e6b7ba199
yamashita-fumiya <asasasafmfmfm@gmail.com>
2020-01-20 21:30:59 +0900

    コメント

---
+++ script.js
@@ -0,0 +1,8 @@
+var a = (5*5)*3.14
+{document.write(a);document.write("<br>");}
+
+var b = (7*7)*3.14
+{document.write(b);document.write("<br>");}
+
+var c = (10*10)*3.14
+{document.write(c);document.write("<br>");}
